a2b074bb40dbfb0965bb62eb1afcc1e1c10933c7
Daniel Everett <deverett@gmail.com>
2023-10-28 20:36:36 +1100

    Split webpack into dev and prod configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -9,7 +10,11 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Bibliograph' }),
+  ],
   module: {
     rules: [
       { test: /\..json$/, use: { loader: 'json-loader' } },
